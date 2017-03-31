c5474d76ec253b7c233a950f8cb4bf803dd4a29f
Optimus_Prime <aup8497@users.noreply.github.com>
2017-03-31 10:11:39 +0000

    added a command $ inspireme

---
+++ inspireQuoteOnBash.py
@@ -0,0 +1,6 @@
+import requests
+r = requests.get('http://quotes.rest/qod.json?category=inspire') 
+quote = r.json()
+# print(quote)
+print(quote["contents"]["quotes"][0]["quote"],end=" \t\t\t\t "),
+print(quote["contents"]["quotes"][0]["author"])
