063feed665568511ecd4bdf8e1fbd0f4adcd5f96
Optimus_Prime <aup8497@users.noreply.github.com>
2017-04-01 12:01:58 +0000

    exception handled

--- quoteOnBash.py
+++ quoteOnBash.py
@@ -1,4 +1,7 @@
 import requests
-r = requests.get('http://quotes.stormconsultancy.co.uk/random.json') 
-quote = r.json()
-print(quote["quote"])
+try:
+	r = requests.get('http://quotes.stormconsultancy.co.uk/random.json') 
+	quote = r.json()
+	print(quote["quote"])
+except Exception as e:
+	print "No quote for you because you deprived me of the internet. Haha."
