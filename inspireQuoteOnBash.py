892beaee98fb2a820f7801d4a4255c1293b275ea
Optimus_Prime <aup8497@users.noreply.github.com>
2017-04-02 08:10:29 +0000

    added exception handling to inspireme command

--- inspireQuoteOnBash.py
+++ inspireQuoteOnBash.py
@@ -1,6 +1,9 @@
 import requests
-r = requests.get('http://quotes.rest/qod.json?category=inspire') 
-quote = r.json()
-# print(quote)
-print(quote["contents"]["quotes"][0]["quote"],end=" \t\t\t\t "),
-print(quote["contents"]["quotes"][0]["author"])
+try:
+	r = requests.get('http://quotes.rest/qod.json?category=inspire') 
+	quote = r.json()
+	# print(quote)
+	print(quote["contents"]["quotes"][0]["quote"],end=" \t\t\t\t "),
+	print(quote["contents"]["quotes"][0]["author"])
+except Exception as e:
+	print ("No internet")
